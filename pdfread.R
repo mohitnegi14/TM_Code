48c464d978321345a00bf15cdd3094d5b7e3a073
Mohit <mohitnegi1442000@gmail.com>
2023-07-27 11:21:25 +0530

    Added code for scraping pdf

---
+++ pdfread.R
@@ -0,0 +1,76 @@
+################## 
+# MOHIT NEGI
+# Last Updated : 27 July 2023
+# Contact on : mohit.negi@studbocconi.it
+################## 
+
+################## PATHS
+raw <- 'C:/Users/anjun/OneDrive/Desktop/EP/TM/TM_Data/Raw'
+cleaned <- 'C:/Users/anjun/OneDrive/Desktop/EP/TM/TM_Data/Cleaned'
+output <- 'C:/Users/anjun/OneDrive/Desktop/EP/TM/TM_Data/Output'
+##################
+
+################## LIBRARIES
+library(tidyverse)
+library(haven)
+library(pdftools)
+library(glue)
+library(stringr)
+library(stringi)
+##################
+
+# First load in the entire pdf.
+pdf_raw <- pdf_text(glue('{raw}/pdftable.pdf'))
+
+pdf_clean <- data.frame('cbo2002' = '', 'occupation' = '', 'cbo94' = '', 'ciuo88' = '')
+# Now loop scraping over the pages.
+for(pagenum in 1:length(pdf_raw)) {
+  
+  # Select just one page.
+  page <- pdf_raw[pagenum] %>% str_split("\n")
+  page <- page[[1]]
+  
+  # Trim to remove unneccessary spaces.
+  page <- page %>% trimws()
+  
+  # Keep only relevant rows, remove top and bottom extra rows.
+  page <- page[(grep("CBO2002 T", page) + 1):length(page)]
+  page <- page[which(page != "")]
+  page <- page[which(page != glue("{pagenum}"))]
+  
+  # Split the columns, doesn't work with first one, we deal with it later.
+  page <- str_split_fixed(page, " {2,}", 3)
+  
+  # Now split first column. Also convert to ASCII characters and remove dashes.
+  col12 <- page[,1]
+  col1 <- str_sub(col12, 1, 7) %>% str_replace_all(pattern = '-', replacement = '')
+  col2 <- str_sub(col12, 9) %>%  stri_trans_general("Latin-ASCII")
+  
+  # Remove dots and dashes from occupational codes.
+  col3 <- page[,2] %>% str_replace_all(pattern = '[:punct:]', replacement = '')
+  col4 <- page[,3]
+  
+  # Combine everything.
+  page <- cbind(col1, col2, col3, col4)
+  
+  # Finally, convert to dataframe.
+  page <- data.frame(page)
+  names(page) <- c('cbo2002', 'occupation', 'cbo94', 'ciuo88')
+  
+  pdf_clean <- rbind(pdf_clean, page)
+
+}
+
+# Remove first dummy row.
+pdf_clean <- pdf_clean[2:nrow(pdf_clean),]
+
+# Save as STATA crosswalk.
+write_dta(pdf_clean, glue('{cleaned}/crosswalk_cbo94_2002.dta'))
+
+
+
+
+
+
+
+
